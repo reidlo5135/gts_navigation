4bf291021dc4f6582c982df0c4a872c7846532f1
reidlo5135 <201840103@email.daelim.ac.kr>
2023-11-04 12:22:52 +0900

    chore : rename launch file name

--- gts_navigator/launch/gts_navigator.py
+++ gts_navigator/launch/gts_navigator.py
@@ -3,17 +3,16 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-def generate_launch_description():
-    ld = LaunchDescription()
+def generate_launch_description() -> LaunchDescription:
+    ld: LaunchDescription = LaunchDescription()
 
-    package_name: str = 'gps_slam_navigation'
+    package_name: str = 'gts_navigator'
     package_shared_directory: str = get_package_share_directory(package_name)
-    executable_name: str = 'navigator'
     
-    navigator_node = Node(
+    navigator_node: Node = Node(
         package=package_name,
-        executable=executable_name,
-        name=executable_name,
+        executable=package_name,
+        name=package_name,
         output='screen',
         parameters=[]
     )
